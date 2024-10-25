a54b668ec3ca852a30d9883692972979fe117ca6
kaareskokebok <kaare@jdforlag.no>
2024-10-25 09:07:57 +0200

    update

--- JavaScript/Del-3/radiobtn-ekstern-script/script.js
+++ JavaScript/Del-3/radiobtn-ekstern-script/script.js
@@ -1,1 +1,13 @@
 // Javascript her
+function sjekkSvar(){
+    // txtUt.textContent = "test";
+    let svar1 = document.querySelector('input[name="radSpm1"]:checked').value;
+
+    txtUt.textContent = `Du svarte ${svar1} på spm1.`;
+
+}
+
+let btnSjekkSvar = document.getElementById("btnSjekkSvar");
+let txtUt = document.getElementById("txtUt");
+
+btnSjekkSvar.addEventListener("click", sjekkSvar);
