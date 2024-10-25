1f14e6478bd4d6e45392842f229dd6b36b06997e
kaareskokebok <kaare@jdforlag.no>
2024-10-25 09:12:27 +0200

    ferdig

--- JavaScript/Del-3/radiobtn-ekstern-script/script.js
+++ JavaScript/Del-3/radiobtn-ekstern-script/script.js
@@ -1,9 +1,22 @@
 // Javascript her
 function sjekkSvar(){
     // txtUt.textContent = "test";
+    // Samle inn svar
     let svar1 = document.querySelector('input[name="radSpm1"]:checked').value;
+    let svar2 = document.querySelector('input[name="radSpm2"]:checked').value;
+    let poeng = 0;
+    // Sjekk svar
+    if (svar1 == "nei") {
+        poeng++;
+    }
+    if (svar2 == "Lage variabler"){
+        poeng++;
+    }
 
-    txtUt.textContent = `Du svarte ${svar1} på spm1.`;
+    
+    txtUt.innerHTML = `Du svarte <strong>${svar1}</strong> på spm1, <br>
+    og <strong>${svar2}</strong> på spm2.<br>
+    Du fikk ${poeng} av 2 mulige.`;
 
 }
 
