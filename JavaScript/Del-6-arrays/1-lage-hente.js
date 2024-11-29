236064ffa08b14dd7db88bb42faa177b88ad9c2c
kaareskokebok <kaare@jdforlag.no>
2024-11-29 12:45:45 +0100

    add array1

---
+++ JavaScript/Del-6-arrays/1-lage-hente.js
@@ -0,0 +1,23 @@
+// Arrayer
+// Installere nodejs for linux
+console.log("test");
+
+// Før array. Lagre venner
+let venn1 = "jens";
+let venn2 = "arne";
+let venn3 = "sara";
+
+// array: variabel med flere verdier
+let venner = ["jens", "arne", "sara"];
+// hente ut verdier fra array
+console.log(venner);
+console.log(venner[0]);  //jens
+console.log(venner[1]);
+console.log(venner[2]);  // sara
+console.log(venner[3]);  // undefined
+
+// Lag et array med fire tall
+// Skriv ut hele arrayet
+// Skriv ut det 2. og det 4. tallet
+// Skriv ut venner[i] der i byttes ut med et tall slik at
+// du får undefined i console.log
